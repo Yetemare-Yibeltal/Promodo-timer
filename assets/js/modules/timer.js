ea33d5fb47c73f0b7ce28883d46327bdabb5fce3
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-09-01 23:06:32 +0300

    Implement core countdown timer logic and mode state handlers

---
+++ assets/js/modules/timer.js
@@ -0,0 +1,60 @@
+import { formatTime } from "../utils/time.js";
+
+export class TimerManager {
+  constructor(config, onTick, onComplete) {
+    this.config = config;
+    this.onTick = onTick;
+    this.onComplete = onComplete;
+    this.currentMode = "work";
+    this.timeRemaining = this.config.modes.work.defaultTime;
+    this.totalDuration = this.timeRemaining;
+    this.intervalId = null;
+    this.isRunning = false;
+  }
+
+  setMode(mode) {
+    this.pause();
+    this.currentMode = mode;
+    this.totalDuration = this.config.modes[mode].defaultTime;
+    this.timeRemaining = this.totalDuration;
+    this.onTick(this.getFormattedTime(), this.getProgressPercentage());
+  }
+
+  start() {
+    if (this.isRunning) return;
+    this.isRunning = true;
+    this.intervalId = setInterval(() => {
+      this.timeRemaining--;
+      this.onTick(this.getFormattedTime(), this.getProgressPercentage());
+
+      if (this.timeRemaining <= 0) {
+        this.pause();
+        this.onComplete(this.currentMode);
+      }
+    }, 1000);
+  }
+
+  pause() {
+    this.isRunning = false;
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  }
+
+  reset() {
+    this.pause();
+    this.timeRemaining = this.totalDuration;
+    this.onTick(this.getFormattedTime(), this.getProgressPercentage());
+  }
+
+  getFormattedTime() {
+    return formatTime(this.timeRemaining);
+  }
+
+  getProgressPercentage() {
+    return (
+      ((this.totalDuration - this.timeRemaining) / this.totalDuration) * 100
+    );
+  }
+}
